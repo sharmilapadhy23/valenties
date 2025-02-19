f8d4f96476870ce8d748f715eafaf2fe6e6bda3c
sharmila Padhy <138507696+sharmilapadhy23@users.noreply.github.com>
2025-02-19 15:24:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,6 +13,6 @@ form.addEventListener('submit', (e) => {
 		resultDiv.innerHTML = 'Baby';
 		window.location.href = nextPageUrl; // Redirect to new page
 	} else {
-		resultDiv.innerHTML = 'Naa Tum meri baby nhi hoo kon hooo tum kaha hai meri Varshanjali';
+		resultDiv.innerHTML = 'Naa Tum meri baby nhi hoo kon hooo tum kaha hai mera Krishna';
 	}
 });
